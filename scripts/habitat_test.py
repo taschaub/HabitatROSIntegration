45a3af3cad1b95ad23098177b51bf80e7330c25e
Aaron Schaub <tim.aaron.schaub@gmail.com>
2023-04-05 11:10:52 +0200

    add top down map

--- scripts/habitat_test.py
+++ scripts/habitat_test.py
@@ -7,6 +7,36 @@ from habitat.config.read_write import read_write
 from threading import Thread
 from queue import Queue
 
+import math
+import numpy as np
+import cv2
+from habitat.utils.visualizations import maps
+
+def display_top_down_map(env):
+    agent_state = env.sim.get_agent_state()
+    top_down_map = maps.get_topdown_map(env.sim.pathfinder, meters_per_pixel=0.1, height=0.5)
+    top_down_map = maps.colorize_topdown_map(top_down_map)
+
+    # Draw the agent's position and orientation on the map
+    agent_pos = agent_state.position
+    agent_rot = agent_state.rotation
+    agent_angle = -2 * math.atan2(agent_rot.y, agent_rot.w)  # Get agent's yaw (rotation around the Z-axis)
+
+    # Convert the agent position to integer values
+    grid_resolution = 0.1
+    agent_pos_grid = (agent_pos - env.sim.pathfinder.get_bounds()[0]) / grid_resolution
+    agent_pos_int = np.round(agent_pos_grid).astype(np.int32)
+
+    top_down_map_with_agent = maps.draw_agent(
+        top_down_map,
+        agent_pos_int,
+        agent_angle,
+        agent_radius_px=8,  # Adjust the agent_radius_px for the size of the agent marker
+        # agent_color=(0, 0, 255)
+    )
+
+    cv2.imshow("Top Down Map", top_down_map_with_agent)
+    cv2.waitKey(1)
 
 def habitat_thread(agent_config, scene, action_queue):
     with read_write(agent_config):
@@ -17,11 +47,13 @@ def habitat_thread(agent_config, scene, action_queue):
     observations = env.reset()
 
     while not rospy.is_shutdown():
+        display_top_down_map(env)
         if not action_queue.empty():
             action = action_queue.get()
             print(action)
             observations = env.step(action)
             print(observations)
+
 
 
 def main():
@@ -46,3 +78,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+   
